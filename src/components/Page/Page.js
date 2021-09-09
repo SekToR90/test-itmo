b9acad504bedf386159e9ab465bc1e24f0e0130b
Nikita Ustimenko <ustimencko.nickita@yandex.ru>
2021-09-09 13:37:42 +0300

    fix styles page.js

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -11,6 +11,23 @@ import GroupIcon from '@material-ui/icons/Group';
 
 
 function Page() {
+    const buttonStyles = {
+        borderRadius: '0', backgroundColor: '#86BD24',
+        textTransform: 'none', justifyContent: 'flex-start', paddingLeft: '27px',
+        marginBottom: '26px'
+    };
+
+    const iconStyles = {
+        color: '#86BD24', backgroundColor: '#fff',
+        padding: '5px', borderRadius: '5px', width: '15px',
+        height: '15px'
+    };
+
+    const iconExitStyles = {
+        backgroundColor: '#E8E8E8', padding: '4px', width: '18px',
+        height: '18px', borderRadius: '5px', transform: 'rotate(-90deg)'
+    };
+
     return (
         <Box display="flex">
             <Box display="flex" flexDirection="column" boxShadow={3} className={classNames(classes.pageMenu)}>
@@ -21,44 +38,29 @@ function Page() {
                         <p className={classNames(classes.team)}>Team member Tube</p>
                     </Box>
                 </Box>
-                <Button style={{
-                    borderRadius: '0', backgroundColor: '#86BD24',
-                    textTransform: 'none', justifyContent: 'flex-start', paddingLeft: '27px',
-                    marginBottom: '26px'
-                }} disableElevation><WorkIcon alt="work-icon" style={{
-                    color: '#86BD24', backgroundColor: '#fff',
-                    padding: '5px', borderRadius: '5px', width: '15px',
-                    height: '15px'
-                }} /><span className={classNames(classes.buttonText)}>Page_1</span>
+                <Button style={buttonStyles} disableElevation>
+                    <WorkIcon alt="work-icon" style={iconStyles} />
+                    <span className={classNames(classes.buttonText)}>Page_1</span>
                 </Button>
-                <Button style={{
-                    borderRadius: '0', backgroundColor: '#86BD24',
-                    textTransform: 'none', justifyContent: 'flex-start', paddingLeft: '27px',
-                    marginBottom: '26px'
-                }} disableElevation><DonutSmallIcon alt="work-icon" style={{
-                    color: '#86BD24', backgroundColor: '#fff',
-                    padding: '5px', borderRadius: '5px', width: '15px',
-                    height: '15px'
-                }} /><span className={classNames(classes.buttonText)}>Page_2</span>
+                <Button style={buttonStyles} disableElevation>
+                    <DonutSmallIcon alt="work-icon" style={iconStyles} />
+                    <span className={classNames(classes.buttonText)}>Page_2</span>
                 </Button>
-                <Button style={{
-                    borderRadius: '0', backgroundColor: '#86BD24',
-                    textTransform: 'none', justifyContent: 'flex-start', paddingLeft: '27px',
-                    marginBottom: '26px'
-                }} disableElevation><GroupIcon alt="work-icon" style={{
-                    color: '#86BD24', backgroundColor: '#fff',
-                    padding: '5px', borderRadius: '5px', width: '15px',
-                    height: '15px'
-                }} /><span className={classNames(classes.buttonText)}>Page_3</span>
+                <Button style={buttonStyles} disableElevation>
+                    <GroupIcon alt="work-icon" style={iconStyles} />
+                    <span className={classNames(classes.buttonText)}>Page_3</span>
                 </Button>
-                <Button disableElevation><ExitToAppIcon style={{
-                    backgroundColor: '#E8E8E8', padding: '4px', width: '18px',
-                    height: '18px', borderRadius: '5px', transform: 'rotate(-90deg)'
-                }} /><span>Main Page</span></Button>
-                <Button disableElevation><ExitToAppIcon style={{
-                    backgroundColor: '#E8E8E8', padding: '4px', width: '18px',
-                    height: '18px', borderRadius: '5px', transform: 'rotate(-180deg)'
-                }} /><span>Logout</span></Button>
+                <Button disableElevation>
+                    <ExitToAppIcon style={iconExitStyles} />
+                    <span>Main Page</span>
+                </Button>
+                <Button disableElevation>
+                    <ExitToAppIcon style={{
+                        ...iconExitStyles,
+                        transform: 'rotate(-180deg)'
+                    }} />
+                    <span>Logout</span>
+                </Button>
             </Box>
             <h1>Page_1</h1>
         </Box>
